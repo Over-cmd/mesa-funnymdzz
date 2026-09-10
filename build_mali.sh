2e010036eec169d75e0a2dd9b134770dbec8ef2b
Over-cmd <sergioleonpines@gmail.com>
2026-09-10 22:36:04 +0200

    Update build_mali.sh to use variable syntax

--- build_mali.sh
+++ build_mali.sh
@@ -74,7 +74,7 @@ export PKG_CONFIG_PATH="$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/lib/pkgco
 envsubst < android.toml > android-cross.txt
 
 # 🟢 ¡AQUÍ MISMO EN MEDIO ENCHÚFALE ESTA LÍNEA DE INYECCIÓN DIRECTA!
-sed -i "s|c_args = \[|c_args = \['-I\$GITHUB_WORKSPACE/shims', |g" android-cross.txt; sed -i "s|cpp_args = \[|cpp_args = \['-I\$GITHUB_WORKSPACE/shims', |g" android-cross.txt
+sed -i "s|c_args = \[|c_args = \['-I${GITHUB_WORKSPACE}/shims', |g" android-cross.txt; sed -i "s|cpp_args = \[|cpp_args = \['-I${GITHUB_WORKSPACE}/shims', |g" android-cross.txt
 
 # Línea 79 de tu archivo original en la web:
 meson setup build --reconfigure --cross-file android-cross.txt --wrap-mode=forcefallback \
