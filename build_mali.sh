4d753a5b6bc5441cec95c9816cbcdad8db1c03d5
Over-cmd <sergioleonpines@gmail.com>
2026-09-10 22:34:00 +0200

    Add include paths to android-cross.txt
    Inject include paths into android-cross.txt for c_args and cpp_args.

--- build_mali.sh
+++ build_mali.sh
@@ -70,9 +70,15 @@ export PKG_CONFIG_FOR_BUILD="/usr/bin/pkg-config"
 export PKG_CONFIG_PATH_FOR_BUILD="/usr/lib/x86_64-linux-gnu/pkgconfig"
 export PKG_CONFIG_PATH="$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/lib/pkgconfig"
 
+# Línea 73 de tu archivo original en la web:
 envsubst < android.toml > android-cross.txt
 
+# 🟢 ¡AQUÍ MISMO EN MEDIO ENCHÚFALE ESTA LÍNEA DE INYECCIÓN DIRECTA!
+sed -i "s|c_args = \[|c_args = \['-I\$GITHUB_WORKSPACE/shims', |g" android-cross.txt; sed -i "s|cpp_args = \[|cpp_args = \['-I\$GITHUB_WORKSPACE/shims', |g" android-cross.txt
+
+# Línea 79 de tu archivo original en la web:
 meson setup build --reconfigure --cross-file android-cross.txt --wrap-mode=forcefallback \
+
     -Ddefault_library=both \
     -Dbuildtype=debugoptimized \
     -Dstrip=false \
